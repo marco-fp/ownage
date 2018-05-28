209beef17023b129b767124caa7b43b2ac0815d7
Fernandez Pranno, Marco <marco.fernandez@r3pi.io>
2018-05-28 16:11:47 +0200

    Block, blockchain, hashing some stuff.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ownage
 My attempt at building a global scalable cryptocurrency-network to rule the world, forever.
+
+### Links
+
+Articles, whitepapers, posts, etc used to code this project.
+
+- https://jeiwan.cc/posts/building-blockchain-in-go-part-1/
---
+++ src/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	bc := NewBlockchain()
+	bc.AddBlock("First block")
+	bc.AddBlock("Second block")
+
+	for _, block := range bc.blocks {
+		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		fmt.Printf("\n")
+	}
+}
