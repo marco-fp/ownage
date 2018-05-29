ce3b7cf69778c0453bc4a27e358cdfcfa4304862
Marco Fernandez Pranno <mfernandezpranno@gmail.com>
2018-05-30 01:25:46 +0200

    Proof of Work, mining and validation of blocks

--- src/main.go
+++ src/main.go
@@ -1,16 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 	bc := NewBlockchain()
 	bc.AddBlock("First block")
 	bc.AddBlock("Second block")
 
+	fmt.Printf("\n")
+
 	for _, block := range bc.blocks {
 		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
 		fmt.Printf("Data: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
-		fmt.Printf("\n")
+		pow := NewProofOfWork(block)
+		fmt.Println("Nonce is: ", block.Nonce)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
 	}
 }
